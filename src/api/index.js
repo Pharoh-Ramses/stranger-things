2de0716194be3e60ffd53558b38fd48c9ab6ff83
Ramses Angles <ramsescangles@gmail.com>
2022-03-12 12:15:18 -0600

    formated the code to look  pretty

--- src/api/index.js
+++ src/api/index.js
@@ -1,31 +1,31 @@
 export const getPosts = async () => {
-    const url = 'https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/posts';
-    const response = await fetch(url)
-    const json = await response.json()
-    return json;
-} 
+  const url =
+    "https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/posts";
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+};
 
-export const registerUser = async (username,password) => {
-    const url = 'https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/users/register';
-    try{
-    const response = await fetch(url,{
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
+export const registerUser = async (username, password) => {
+  const url =
+    "https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/users/register";
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password,
         },
-        body: JSON.stringify({
-          user: {
-            username: username,
-            password: password
-          }
-        })
-       
-    })
-    const json = await response.json()
+      }),
+    });
+    const json = await response.json();
     console.log(json);
     return json;
-} catch (err){
-    console.error('Could not register user',err)
-}
-}
-
+  } catch (err) {
+    console.error("Could not register user", err);
+  }
+};
